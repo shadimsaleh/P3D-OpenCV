f3462236da05d869e219a40625935948af500931
Rui Caridade <ruidanielcaridade@gmail.com>
2016-05-23 15:07:43 +0100

    Added GLEW and SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <opencv2\opencv.hpp>
 #include <opencv\highgui.h>
+#include <SFML\Graphics.hpp>
+#include <GL\glew.h>
 
 using namespace cv;
 
@@ -8,6 +10,32 @@ void saturate(Mat* mat) {
 	Scalar intensity = mat->at<uchar>(0, 1);
 }
 
+int main() {
+	sf::Window window(sf::VideoMode(800, 600), "OpenGL");
+
+	bool running = true;
+	while (running)
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				running = false;
+			}
+			else if (event.type == sf::Event::Resized)
+			{
+				glViewport(0, 0, event.size.width, event.size.height);
+			}
+		}
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		window.display();
+	}
+}
+
+#if 0
 int main() {
 	Mat image;
 	VideoCapture cap;
@@ -23,3 +51,4 @@ int main() {
 	}
 	return 0;
 }
+#endif
